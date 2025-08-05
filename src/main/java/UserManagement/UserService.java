a178691bd981164bcbcfe0893cdeb4f27f017923
GallantJoseph <gallant.joseph@outlook.com>
2025-08-05 11:16:11 -0230

    UserDAO, UserService, Trainer Menu updates
    -Main: added testing values for admin, member, trainer users
    -Menu: Login, Register, TrainerMenu basic template
    -UserDAO: template for CRUD
    -UserService: login and register a user template

--- src/main/java/UserManagement/UserService.java
+++ src/main/java/UserManagement/UserService.java
@@ -1,4 +1,15 @@
 package UserManagement;
 
+import Users.User;
+
 public class UserService {
+    public static void login(String username, String password) {
+        // Menu for user login
+        System.out.println("User " + username + " logged in successfully.");
+    }
+
+    public static void register(User user) {
+        // Menu for user registration
+        UserDAO.createUser(user);
+    }
 }
